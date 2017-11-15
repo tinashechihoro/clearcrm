702028b3c4ea041ca41dcdb7e0ea94b834df8829
tinashechihoro@gmail.com <11glasspabbles!@#$%Q>
2017-11-15 16:14:27 +0200

    created the subscriber view

--- clearcrmproject/subscribers/views.py
+++ clearcrmproject/subscribers/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import SubscribeForm
+
+def subscriber_new(request,template='subscribers/subscriber_new.html'):
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+           # Unpack form values
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            email = form.cleaned_data['email']
+           #Create user record
+            user = User(username=username,email=email)
+            user.set_password(password)
+            user.save()
+            return  HttpResponseRedirect('/success/')
+
+    else:
+        form = SubscribeForm()
+
+    return render(request,template,{'form':form})
+
+
